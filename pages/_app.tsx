3e9a8b8c6ca91b5f81308d6ff0383f6a1c4cce58
m4chado <enzo653fm@gmail.com>
2024-01-28 13:32:40 -0300

    feat: implement global styles with createGlobalStyle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,20 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import GlobalStyles from "../styles/global";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Centro Pokémon</title>
+				<meta
+					name="description"
+					content="A web project developed with NextJS and TypeScript for a front-end test"
+				/>
+			</Head>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</>
+	);
 }
+
