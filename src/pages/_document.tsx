83e9c9e5581cc6a1a2dd3124be3a9be80dcc57f1
m4chado <enzo653fm@gmail.com>
2024-01-30 15:12:43 -0300

    feat: add custom fonts in app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,7 +40,21 @@ export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="pt-BR">
-				<Head />
+				<Head>
+					<link
+						rel="preconnect"
+						href="https://fonts.googleapis.com"
+					/>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin=""
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
