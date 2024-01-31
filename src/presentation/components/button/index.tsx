a9d45688c0994059711f3a81122933a9089653c8
m4chado <enzo653fm@gmail.com>
2024-01-31 14:49:32 -0300

    feat: create outline button variant

--- src/presentation/components/button/index.tsx
+++ src/presentation/components/button/index.tsx
@@ -3,14 +3,15 @@ import * as S from "./styles";
 
 export interface ButtonProps
 	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	variant?: "default" | "ghost";
+	variant?: "default" | "ghost" | "outline";
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-	({ children, ...props }, ref) => {
+	({ children, type = "button", ...props }, ref) => {
 		return (
 			<S.Button
 				ref={ref}
+				type={type}
 				{...props}
 			>
 				{children}
--- src/presentation/components/button/styles.ts
+++ src/presentation/components/button/styles.ts
@@ -20,11 +20,24 @@ const ghostStyles = css`
 	}
 `;
 
+const outlineStyles = css`
+	font-weight: var(--font-bold);
+	font-size: var(--text-md);
+	background: none;
+	border: 1px solid var(--black);
+
+	&:hover {
+		background-color: var(--gray-0);
+	}
+`;
+
 export const Button = styled.button<ButtonProps>`
 	height: 45px;
+	width: fit-content;
 	padding: 16px 20px;
 	border-radius: 30px;
 	display: flex;
+	gap: 10px;
 	align-items: center;
 	white-space: nowrap;
 	font-size: var(--text-lg);
@@ -32,6 +45,11 @@ export const Button = styled.button<ButtonProps>`
 	border: none;
 	cursor: pointer;
 
-	${({ variant }) => (variant === "ghost" ? ghostStyles : defaultStyles)}
+	${({ variant }) =>
+		variant === "ghost"
+			? ghostStyles
+			: variant === "outline"
+			? outlineStyles
+			: defaultStyles}
 `;
 
