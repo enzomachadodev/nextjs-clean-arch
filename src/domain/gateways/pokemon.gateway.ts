96808129486a853a578700d483bb93ebab0dbcde
m4chado <enzo653fm@gmail.com>
2024-01-31 19:00:02 -0300

    feat: create PokemonGateway interface

---
+++ src/domain/gateways/pokemon.gateway.ts
@@ -0,0 +1,5 @@
+import { PokemonEntity } from "../entities/pokemon.entity";
+import { GatewayInterface } from "./gateway-contracts";
+
+export interface PokemonGateway extends GatewayInterface<PokemonEntity> {}
+
