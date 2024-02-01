fba6729c1351e64a1440218ed4b90c3db6599408
m4chado <enzo653fm@gmail.com>
2024-02-01 01:12:20 -0300

    feat: create GetRegionUseCase usecase

---
+++ src/application/usecases/get-region.usecase.ts
@@ -0,0 +1,21 @@
+import { RegionGateway } from "@/domain/gateways/region.gateway";
+import { UseCase as DefaultUseCase } from "@/domain/usecases/usecase";
+import { RegionOutput, RegionOutputMapper } from "../dtos/region-output";
+
+export namespace GetRegionUseCase {
+	export type Input = {
+		name: string;
+	};
+
+	export type Output = RegionOutput;
+
+	export class UseCase implements DefaultUseCase<Input, Output> {
+		constructor(private regionGateway: RegionGateway) {}
+
+		async execute(input: Input): Promise<Output> {
+			const entity = await this.regionGateway.findByName(input.name);
+			return RegionOutputMapper.toOutput(entity);
+		}
+	}
+}
+
