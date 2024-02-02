74c9dac291aab89d7310093032be2c6c7fdb0961
m4chado <enzo653fm@gmail.com>
2024-02-02 14:14:35 -0300

    refactor: move form component to layout

--- src/presentation/layouts/schedule-appointment/index.tsx
+++ src/presentation/layouts/schedule-appointment/index.tsx
@@ -1,15 +1,226 @@
-import * as S from "./styles";
+import Image from "next/image";
+import { use, useEffect, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Form } from "@/presentation/components/form";
+import { Option } from "@/presentation/components/select";
+import { Button } from "@/presentation/components/button";
 import { PageHeader } from "@/presentation/components/page-header";
-import { ScheduleAppointmentForm } from "@/presentation/components/schedule-appointment-form";
+import { ControlledInput } from "@/presentation/components/controlled-input";
+import { ControlledSelect } from "@/presentation/components/controlled-select";
+import { useLocationOptions } from "@/presentation/hooks/use-location-options";
+import { useTimeOptions } from "@/presentation/hooks/use-time-options";
+import {
+	CreateScheduleFormData,
+	createScheduleSchema,
+} from "@/presentation/schemas/create-schedule-schema";
+import * as S from "./styles";
 
-export const ScheduleAppointmentLayout: React.FC = () => {
+interface ScheduleAppointmentLayoutProps {
+	dates: Option[];
+	pokemons: Option[];
+	regions: Option[];
+}
+
+export const ScheduleAppointmentLayout: React.FC<
+	ScheduleAppointmentLayoutProps
+> = ({ dates, pokemons, regions }) => {
+	const [pokemonCount, setPokemonCount] = useState<number>(1);
+	const formMethods = useForm<CreateScheduleFormData>({
+		resolver: yupResolver(createScheduleSchema),
+	});
+
+	const { watch, reset, setValue, setError, formState } = formMethods;
+
+	useEffect(() => {
+		console.log(formState.errors);
+	}, [formState.errors]);
+
+	const regionValue = watch("region");
+	const dateValue = watch("date");
+	const firstPokemonValue = watch(`pokemons.${0}`);
+
+	const {
+		data: locationOptions = [],
+		isFetching: loadingLocationOptions,
+		refetch: refetchLocation,
+	} = useLocationOptions(regionValue);
+
+	const {
+		data: timeOptions = [],
+		isFetching: loadingTimeOptions,
+		refetch: refetchTime,
+	} = useTimeOptions(regionValue);
+
+	const handleSelectRegion = () => {
+		setValue("location", "");
+		refetchLocation();
+	};
+
+	const addPokemonInput = () => {
+		if (pokemonCount < 6) {
+			setPokemonCount((prevCount) => prevCount + 1);
+		}
+	};
+
+	const removePokemonInput = () => {
+		if (pokemonCount > 1) {
+			setPokemonCount((prevCount) => prevCount - 1);
+			setValue(`pokemons.${pokemonCount - 1}`, "");
+		}
+	};
+
+	const onSubmit: SubmitHandler<CreateScheduleFormData> = (data) => {
+		if (!firstPokemonValue)
+			return setError(`pokemons.${0}`, {
+				type: "required",
+				message: "Selecione no mínimo um pokemon",
+			});
+		setPokemonCount(1);
+		reset();
+	};
+
 	return (
-		<S.Container>
-			<PageHeader subtitle="Recupere seus pokémons em 5 segundos" />
-			<S.ContentWrapper>
-				<ScheduleAppointmentForm />
-			</S.ContentWrapper>
-		</S.Container>
+		<>
+			<S.Container>
+				<PageHeader subtitle="Recupere seus pokémons em 5 segundos" />
+				<S.ContentWrapper>
+					<Form
+						onSubmit={onSubmit}
+						customFormMethods={formMethods}
+					>
+						<S.FormContainer>
+							<S.Title>
+								Preencha o formulário abaixo para agendar sua consulta
+							</S.Title>
+							<S.CustomerDataContainer>
+								<S.InputRowContainer>
+									<ControlledInput
+										name="name"
+										label="Nome"
+										placeholder="Digite seu nome"
+										defaultValue=""
+									/>
+									<ControlledInput
+										name="surname"
+										label="Sobrenome"
+										placeholder="Digite seu sobrenome"
+										defaultValue=""
+									/>
+								</S.InputRowContainer>
+								<S.InputRowContainer>
+									<ControlledSelect
+										options={regions}
+										name="region"
+										label="Região"
+										placeholder="Selecione sua região"
+										onSelect={handleSelectRegion}
+									/>
+									<ControlledSelect
+										options={locationOptions}
+										name="location"
+										label="Cidade"
+										placeholder="Selecione sua cidade"
+										loading={loadingLocationOptions}
+										disabled={!regionValue}
+									/>
+								</S.InputRowContainer>
+								<S.TeamDataContainer>
+									<S.TeamDataTitle>
+										<span className="title">Cadastre seu time</span>
+										<span className="subtitle">
+											Atendemos até 06 pokémons por vez
+										</span>
+									</S.TeamDataTitle>
+									{[...Array(pokemonCount)].map((_, index) => (
+										<S.RowSelectContainer key={index}>
+											<label className="label">{`Pokémon 0${index + 1}`}</label>
+											<div className="select">
+												<ControlledSelect
+													options={pokemons}
+													name={`pokemons.${index}`}
+													placeholder="Selecione seu pokémon"
+												/>
+											</div>
+											{index + 1 === pokemonCount && index !== 0 ? (
+												<button
+													onClick={() => removePokemonInput()}
+													type="button"
+													className="remove-button"
+												>
+													<Image
+														src="/remove-item.svg"
+														alt="Remove item"
+														width={22}
+														height={22}
+													/>
+												</button>
+											) : (
+												<div className="button-space" />
+											)}
+										</S.RowSelectContainer>
+									))}
+
+									<Button
+										variant="outline"
+										onClick={addPokemonInput}
+									>
+										<span>Adicionar novo pokémon ao time...</span>
+										<span>+</span>
+									</Button>
+								</S.TeamDataContainer>
+								<S.InputRowContainer>
+									<ControlledSelect
+										options={dates}
+										name="date"
+										label="Data para atendimento"
+										placeholder="Selecione uma data"
+										onSelect={() => refetchTime()}
+									/>
+									<ControlledSelect
+										options={timeOptions}
+										name="time"
+										label="Horário de atendimento"
+										placeholder="Selecione um horário"
+										loading={loadingTimeOptions}
+										disabled={!dateValue}
+									/>
+								</S.InputRowContainer>
+								<S.Separator />
+								<S.CheckoutInfo>
+									<div className="row">
+										<p>Número de pokémons a serem atendidos:</p>
+										<p>{"numbers"}</p>
+									</div>
+									<div className="row">
+										<p>Atendimento unitário por pokémon:</p>
+										<p>{"numbers"}</p>
+									</div>
+									<div className="row">
+										<p>Subtotal:</p>
+										<p>{"numbers"}</p>
+									</div>
+									<div className="row">
+										<p>Taxa geracional*:</p>
+										<p>{"numbers"}</p>
+									</div>
+									<p className="tax-info">
+										*adicionamos uma taxa de 3%, multiplicado pelo número da
+										geração mais alta do time, com limite de até 30%
+									</p>
+								</S.CheckoutInfo>
+								<S.CheckoutTotal>
+									<span className="total-value">
+										{`Valor Total: ${"valor total"}`}
+									</span>
+									<Button type="submit">Concluir Agendamento</Button>
+								</S.CheckoutTotal>
+							</S.CustomerDataContainer>
+						</S.FormContainer>
+					</Form>
+				</S.ContentWrapper>
+			</S.Container>
+		</>
 	);
 };
 
--- src/presentation/layouts/schedule-appointment/styles.ts
+++ src/presentation/layouts/schedule-appointment/styles.ts
@@ -15,3 +15,133 @@ export const ContentWrapper = styled(MaxWidthWrapper)`
 	padding-bottom: 64px;
 `;
 
+export const Title = styled.h2`
+	font-size: var(--text-2xl);
+	font-weight: var(--font-semibold);
+	text-align: center;
+`;
+
+export const FormContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 2rem;
+`;
+
+export const InputRowContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	gap: 20px;
+`;
+
+export const CustomerDataContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+	margin-bottom: 80px;
+	max-width: 540px;
+`;
+
+export const Separator = styled.div`
+	width: 100%;
+	height: 1px;
+	background-color: var(--gray-2);
+`;
+
+export const TeamDataContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+`;
+
+export const TeamDataTitle = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+
+	.title {
+		font-size: var(--text-md);
+		font-weight: var(--font-bold);
+	}
+
+	.subtitle {
+		font-size: var(--text-md);
+		font-weight: var(--font-medium);
+		color: var(--gray-3);
+	}
+`;
+
+export const RowSelectContainer = styled.div`
+	width: 100%;
+	display: flex;
+	gap: 1rem;
+
+	.label {
+		font-size: var(--text-md);
+		font-weight: var(--font-bold);
+		white-space: nowrap;
+		margin-right: 1rem;
+		margin-top: 15px;
+	}
+
+	.remove-button {
+		border: none;
+		background: none;
+		opacity: 0.7;
+		cursor: pointer;
+		height: fit-content;
+		margin-top: 12px;
+	}
+
+	.button-space {
+		width: 28px;
+		height: 28px;
+	}
+
+	.select {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.remove-button:hover {
+		opacity: 1;
+	}
+`;
+
+export const CheckoutInfo = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	width: 100%;
+
+	.row {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		font-size: var(--text-lg);
+		font-weight: var(--font-normal);
+		color: var(--gray-3);
+	}
+
+	.tax-info {
+		font-size: var(--text-xs);
+		font-weight: var(--font-normal);
+		color: var(--gray-3);
+	}
+`;
+
+export const CheckoutTotal = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	.total-value {
+		font-size: var(--text-2xl);
+		font-weight: var(--font-semibold);
+	}
+`;
+
