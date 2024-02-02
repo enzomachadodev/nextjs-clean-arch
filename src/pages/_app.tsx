0ba3c63b9891f7da14fd2933ba2475c84b86f5f1
m4chado <enzo653fm@gmail.com>
2024-02-02 14:15:03 -0300

    feat: add QueryClientProvider in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,16 @@ import { GlobalStyles } from "@/presentation/styles/global";
 import { StyleSheetManager } from "styled-components";
 import { Header } from "@/presentation/components/header";
 import { Footer } from "@/presentation/components/footer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: false,
+		},
+	},
+});
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
@@ -15,12 +25,14 @@ export default function App({ Component, pageProps }: AppProps) {
 					content="A web project developed with NextJS and TypeScript for a front-end test"
 				/>
 			</Head>
-			<StyleSheetManager shouldForwardProp={(prop) => prop !== "variant"}>
-				<GlobalStyles />
-				<Header />
-				<Component {...pageProps} />
-				<Footer />
-			</StyleSheetManager>
+			<QueryClientProvider client={queryClient}>
+				<StyleSheetManager shouldForwardProp={(prop) => prop !== "variant"}>
+					<GlobalStyles />
+					<Header />
+					<Component {...pageProps} />
+					<Footer />
+				</StyleSheetManager>
+			</QueryClientProvider>
 		</>
 	);
 }
