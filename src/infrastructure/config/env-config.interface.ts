501fae7664d41779d7cbc3bcc67c775c771c4e2c
m4chado <enzo653fm@gmail.com>
2024-02-02 14:06:48 -0300

    feat: create EntConfig interface

---
+++ src/infrastructure/config/env-config.interface.ts
@@ -0,0 +1,5 @@
+export interface EnvConfig {
+	NEXT_API_URL: string;
+	POKE_API_URL: string;
+}
+
---
+++ src/infrastructure/config/env-config.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import * as yup from "yup";
+import { EnvConfig } from "./env-config.interface";
+
+const envSchema = yup.object({
+	NEXT_API_URL: yup.string().default("http://localhost:3000/api"),
+	POKE_API_URL: yup.string().default("https://pokeapi.co/api/v2"),
+});
+
+let env: EnvConfig;
+
+try {
+	const _env = envSchema.validateSync(process.env, { abortEarly: false });
+	env = _env;
+} catch (error) {
+	console.error("Invalid enviroment variables.");
+	throw new Error("Invalid enviroment variables.");
+}
+
+export { env };
+
