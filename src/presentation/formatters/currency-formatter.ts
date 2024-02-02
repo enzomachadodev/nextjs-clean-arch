38b0613fd07b903ad02c053856d955b4badd237e
m4chado <enzo653fm@gmail.com>
2024-02-02 14:11:48 -0300

    feat: create currencyFormatter formatter function

---
+++ src/presentation/formatters/currency-formatter.ts
@@ -0,0 +1,9 @@
+export const currencyFormatter = (value: number): string => {
+	return Intl.NumberFormat("pt-BR", {
+		style: "currency",
+		currency: "BRL",
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2,
+	}).format(value);
+};
+
