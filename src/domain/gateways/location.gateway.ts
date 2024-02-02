4914474b9c080aa70f1df857b25ce4a4550a7bfc
m4chado <enzo653fm@gmail.com>
2024-02-02 13:34:34 -0300

    fix: remove LocationGateway class

--- src/domain/gateways/location.gateway.ts
+++
@@ -1,5 +0,0 @@
-import { LocationEntity } from "../entities/location.entity";
-import { GatewayInterface } from "./gateway-contracts";
-
-export interface LocationGateway extends GatewayInterface<LocationEntity> {}
-
