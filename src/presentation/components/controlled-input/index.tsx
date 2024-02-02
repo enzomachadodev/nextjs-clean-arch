8f53130b395f0a5a65f42ea20887cde52a1bdec5
m4chado <enzo653fm@gmail.com>
2024-02-02 14:13:03 -0300

    feat: create ControlledInput component

--- src/presentation/components/controlled-input/index.tsx
+++ src/presentation/components/controlled-input/index.tsx
@@ -8,19 +8,24 @@ import { Input, InputProps } from "../input";
 
 export const ControlledInput = <FormType extends FieldValues>({
 	name,
+	defaultValue,
 	...props
 }: UseControllerProps<FormType> & InputProps) => {
-	const { control } = useFormContext();
+	const { control, setValue } = useFormContext();
 
 	return (
 		<Controller
 			control={control}
 			name={name}
+			defaultValue={defaultValue}
 			render={({ field, fieldState }) => (
 				<Input
 					value={field.value}
 					onBlur={field.onBlur}
-					onChange={field.onChange}
+					onChange={(e) => {
+						setValue(name, e.target.value as any);
+						field.onChange(e);
+					}}
 					error={!!fieldState.error}
 					errorMessage={fieldState.error?.message}
 					{...props}
